cd3011076e425a280214b27fefc8c4fe00e2331b
lhaa0130-hash <lhaa0130@gmail.com>
2025-11-06 01:41:51 +0900

    Create getPostContent.ts

---
+++ lib/getPostContent.ts
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const postsDirectory = path.join(process.cwd(), 'content');
+
+export function getPostContent(slug: string) {
+  const fullPath = path.join(postsDirectory, `${slug}.md`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  // Combine the data with the slug and content
+  return {
+    slug,
+    title: matterResult.data.title as string,
+    date: matterResult.data.date as string,
+    content: matterResult.content, // 글 본문 내용
+  };
+}
+
+// Next.js가 어떤 글 주소들이 있는지 미리 알 수 있도록 모든 글의 주소(slug) 목록을 가져옵니다.
+export function getAllPostSlugs() {
+  const fileNames = fs.readdirSync(postsDirectory);
+  return fileNames.map((fileName) => {
+    return {
+      slug: fileName.replace(/\.md$/, ''),
+    };
+  });
+}
